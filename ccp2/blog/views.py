4b1ea311ccacb43f95f1f285cc1106204fd3f535
annick17 <annick2525@orange.fr>
2020-04-28 18:32:38 +0200

    suppression image, bouton ajout modif fonctionnel

--- ccp2/blog/views.py
+++ ccp2/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
@@ -20,7 +20,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -35,7 +35,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
