4a24ba9d1e581478cb4f8801f628fe304dd90798
annick17 <annick2525@orange.fr>
2020-05-03 16:14:45 +0200

    modif models

--- ccp2/blog/models.py
+++ ccp2/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 # Create your models here.
@@ -7,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=45)
     auteur = models.CharField(max_length=25, default=True)
-    accroche = models.CharField(max_length=45, default=True)
+    accroche = models.CharField(max_length=200, default=True)
     text = models.TextField()  
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
